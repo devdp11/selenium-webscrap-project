fdef78e7c83ccd5ec3ed7dbd16a02b8859441c04
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2024-06-06 19:58:26 +0100

    - loads the information into the csv file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import csv
+import uuid
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium import webdriver
@@ -11,68 +13,74 @@ driver.maximize_window()
 
 time.sleep(1)
 
-movies = driver.find_elements(By.CSS_SELECTOR, 'a.ipc-title-link-wrapper')
+with open('data.csv', mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(["uuid", "movie_name", "movie_rating", "movie_genre", "comment_user", "comment_date", "comment_text"])
 
-for index in range(len(movies)):
     movies = driver.find_elements(By.CSS_SELECTOR, 'a.ipc-title-link-wrapper')
-    movie_link = movies[index]
-    
-    try:
-        driver.execute_script("arguments[0].scrollIntoView(true);", movie_link)
-        driver.execute_script("arguments[0].click();", movie_link)
-        
-        time.sleep(1)
-        print(f"Success on: open movie {index + 1}")
+
+    for index in range(len(movies)):
+        movies = driver.find_elements(By.CSS_SELECTOR, 'a.ipc-title-link-wrapper')
+        movie_link = movies[index]
         
         try:
-            movie_rating = driver.find_element(By.CSS_SELECTOR, 'span[class="sc-bde20123-1 cMEQkK"]').text
-            movie_name = driver.find_element(By.CSS_SELECTOR, 'span[class="hero__primary-text"]').text
-            genre_elements = driver.find_elements(By.CSS_SELECTOR, 'div.ipc-chip-list__scroller a.ipc-chip span.ipc-chip__text')
-            genres = "; ".join([genre.text for genre in genre_elements])
-
-            print(f"Movie name: {movie_name}")
-            print(f"Movie rating: {movie_rating}")
-            print(f"Movie genres: {genres}")
-
-            user_reviews_span = driver.find_element(By.CSS_SELECTOR, 'span.three-Elements > span.score')
-            driver.execute_script("arguments[0].scrollIntoView(true);", user_reviews_span)
-            user_reviews_span.click()
+            driver.execute_script("arguments[0].scrollIntoView(true);", movie_link)
+            driver.execute_script("arguments[0].click();", movie_link)
             
             time.sleep(1)
-            print(f"Success on: user reviews for movie {index + 1}")
-
+            print(f"Success on: open movie {index + 1}")
+            
             try:
-                user_reviews_texts = driver.find_elements(By.CSS_SELECTOR, 'li.ipl-content-list__item')
-                for user_review in user_reviews_texts:
-                    try:
-                        time.sleep(1)
-                        user_name = user_review.find_element(By.CSS_SELECTOR, 'a.display-name-link')
-                        comment_date = user_review.find_element(By.CSS_SELECTOR, 'span.review-date')
-                        
-                        comment_text_element = user_review.find_element(By.CSS_SELECTOR, 'div.text')
-                        comment_text = comment_text_element.text
-                        comment_text = comment_text.split('.')[0] if isinstance(comment_text, str) else comment_text
-                        comment_text = comment_text.split('!')[0] if isinstance(comment_text, str) else comment_text
-                        comment_text = comment_text.split('\n')[0] if isinstance(comment_text, str) else comment_text
-                        
-                        if comment_text.strip():
-                            print(f"User comment by: {user_name.text}")
-                            print(f"Comment date: {comment_date.text}")
-                            print(f"Comment text: {comment_text}")
+                movie_rating = driver.find_element(By.CSS_SELECTOR, 'span[class="sc-bde20123-1 cMEQkK"]').text
+                movie_name = driver.find_element(By.CSS_SELECTOR, 'span[class="hero__primary-text"]').text
+                genre_elements = driver.find_elements(By.CSS_SELECTOR, 'div.ipc-chip-list__scroller a.ipc-chip span.ipc-chip__text')
+                genres = "; ".join([genre.text for genre in genre_elements])
 
-                    except Exception as e:
-                        print(f"Error retrieving user name or comment text for movie {index + 1}: {e}")
+                print(f"Movie name: {movie_name}")
+                print(f"Movie rating: {movie_rating}")
+                print(f"Movie genres: {genres}")
 
-                driver.back()
+                user_reviews_span = driver.find_element(By.CSS_SELECTOR, 'span.three-Elements > span.score')
+                driver.execute_script("arguments[0].scrollIntoView(true);", user_reviews_span)
+                user_reviews_span.click()
+                
+                time.sleep(1)
+                print(f"Success on: user reviews for movie {index + 1}")
+
+                try:
+                    user_reviews_texts = driver.find_elements(By.CSS_SELECTOR, 'li.ipl-content-list__item')
+                    for user_review in user_reviews_texts:
+                        try:
+                            time.sleep(1)
+                            user_name = user_review.find_element(By.CSS_SELECTOR, 'a.display-name-link')
+                            comment_date = user_review.find_element(By.CSS_SELECTOR, 'span.review-date')
+                            
+                            comment_text_element = user_review.find_element(By.CSS_SELECTOR, 'div.text')
+                            comment_text = comment_text_element.text
+                            comment_text = comment_text.split('.')[0] if isinstance(comment_text, str) else comment_text
+                            comment_text = comment_text.split('!')[0] if isinstance(comment_text, str) else comment_text
+                            comment_text = comment_text.split('\n')[0] if isinstance(comment_text, str) else comment_text
+                            
+                            if comment_text.strip():
+                                print(f"User comment by: {user_name.text}")
+                                print(f"Comment date: {comment_date.text}")
+                                print(f"Comment text: {comment_text}")
+                                
+                                writer.writerow([str(uuid.uuid4()), movie_name, movie_rating, genres, user_name.text, comment_date.text, comment_text])
+
+                        except Exception as e:
+                            print(f"Error retrieving user name or comment text for movie {index + 1}: {e}")
+
+                    driver.back()
+                except Exception as e:
+                    print(f"Error on: user comments for movie {index + 1}: {e}")
+
             except Exception as e:
-                print(f"Error on: user comments for movie {index + 1}: {e}")
+                print(f"Error on: user reviews for movie {index + 1}: {e}")
 
+            driver.back()
         except Exception as e:
-            print(f"Error on: user reviews for movie {index + 1}: {e}")
-
-        driver.back()
-    except Exception as e:
-        print(f"Error on: open movie {index + 1}: {e}")
-        driver.back()
+            print(f"Error on: open movie {index + 1}: {e}")
+            driver.back()
 
 driver.quit()
