2d3e86151db07d9c88c9f997d40601dc0446c1b9
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2024-06-06 14:55:43 +0100

    refactored code | changed chromedriver version

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-import time
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
 driver.get("https://thefork.pt")
 driver.maximize_window()
+
+locations = ["Braga", "Ponte de Lima", "Viana do Castelo", "Porto", "Amares"]
