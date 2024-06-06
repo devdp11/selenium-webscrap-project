d6b865f96f9399d3459cbbe6b4f7f381e5155ce6
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2024-06-06 16:45:51 +0100

    - now opens the movies of the populars movies and open the comment section - returns to the main page and open the next movie

--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium import webdriver
+import time
 
 service = Service(executable_path="chromedriver.exe")
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://thefork.pt")
+driver.get("https://m.imdb.com/chart/moviemeter/")
 driver.maximize_window()
 
-locations = ["Braga", "Ponte de Lima", "Viana do Castelo", "Porto", "Amares"]
+movies = driver.find_elements(By.CSS_SELECTOR, 'a.ipc-title-link-wrapper')
+
+for index in range(len(movies)):
+    movies = driver.find_elements(By.CSS_SELECTOR, 'a.ipc-title-link-wrapper')
+    movie_link = movies[index]
+    
+    try:
+        driver.execute_script("arguments[0].scrollIntoView(true);", movie_link)
+        driver.execute_script("arguments[0].click();", movie_link)
+        
+        print(f"Succe on: open movie {index + 1}")
+        
+        try:
+            user_reviews_span = driver.find_element(By.CSS_SELECTOR, 'span.three-Elements > span.score')
+            driver.execute_script("arguments[0].scrollIntoView(true);", user_reviews_span)
+            user_reviews_span.click()
+            
+            print(f"Succe on: user reviews for movie {index + 1}")
+            driver.back()
+        except Exception as e:
+            print(f"Error on: user reviews for movie {index + 1}: {e}")
+
+        driver.back()
+    except Exception as e:
+        print(f"Error on: open movie {index + 1}: {e}")
+        driver.back()
+
+driver.quit()
