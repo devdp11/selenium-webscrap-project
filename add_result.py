2d83ddd1dd2dfb6548b944b6366121dc5dcd33a9
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2024-06-09 15:58:37 +0100

    - changed csv file - created python script to add the attribute result

---
+++ add_result.py
@@ -0,0 +1,23 @@
+import csv
+
+input_file = './assets/data.csv'
+output_file = './assets/data_final.csv'
+
+with open(input_file, mode='r', newline='', encoding='utf-8') as infile:
+    reader = csv.reader(infile)
+    header = next(reader)
+    
+    new_header = header[:1] + ['result'] + header[1:]
+    
+    rows = []
+    
+    for row in reader:
+        new_row = row[:1] + [''] + row[1:]
+        rows.append(new_row)
+
+with open(output_file, mode='w', newline='', encoding='utf-8') as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(new_header)
+    writer.writerows(rows)
+
+print(f'Arquivo CSV modificado foi salvo como {output_file}')
